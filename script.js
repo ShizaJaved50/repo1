cb2669110836d6fb957af94f067d03bb327a2cbc
ShizaJaved50 <165871148+ShizaJaved50@users.noreply.github.com>
2024-04-04 10:00:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const searchIcon = document.querySelector(".search-icon");
+const searchForm = document.querySelector(".search-form");
+
+searchIcon.addEventListener("click", () => {
+    searchForm.classList.add("active");
+    cartitemsContainer.classList.remove("active");
+});
+
+const cartIcon = document.querySelector(".cart-icon");
+const cartitemsContainer = document.querySelector(".cart-items-container");
+
+cartIcon.addEventListener("click", () => {
+    cartitemsContainer.classList.add("active");
+    searchForm.classList.remove("active");
+});
+
+var swiper = new Swiper(".wrapper", {
+    spaceBetween: 10,
+    loop:true,
+    CenterSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+       
+      },
+      1024: {
+        slidesPerView: 3,
+        
+      },
+    },
+  });
